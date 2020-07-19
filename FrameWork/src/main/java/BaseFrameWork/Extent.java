4737d923a17371489d06631bce6c94f07550189b
ArputhaJerrin <60972490+ArputhaJerrin@users.noreply.github.com>
2020-07-19 18:36:02 +0530

    Update Extent.java

--- FrameWork/src/main/java/BaseFrameWork/Extent.java
+++ FrameWork/src/main/java/BaseFrameWork/Extent.java
@@ -13,16 +13,16 @@ public class Extent extends Mail{
 	static ExtentTest test;
 	static ExtentReports report;
 
-	static String filename = "SVEP4158_MAHINDRA_FOTA_WEB_ATMTN_REPORT_" + timestamp + ".html";
+	static String filename = "ATMTN_REPORT_" + timestamp + ".html";
 
 	@BeforeTest
 	public static void start() {
 		
-		report = new ExtentReports("D:\\EMP 878 AJ\\Reports\\FOTA\\" + filename);
+		report = new ExtentReports("File Path" + filename);
 		report.addSystemInfo("User Name", "Arputha Jerrin");
-		report.assignProject("Mahindra FOTA");
-		report.addSystemInfo("Project ID", "SVEP4158");
-		report.addSystemInfo("Project Name", "Mahindra FOTA");
+		report.assignProject("Project Name");
+		report.addSystemInfo("Project ID", "Project ID");
+		report.addSystemInfo("Project Name", "Project Name");
 		report.addSystemInfo("Platform", "Web");
 		report.addSystemInfo("APK Build", "");
 		report.loadConfig(new File(System.getProperty("user.dir") + "/extent-config.xml"));
@@ -33,11 +33,7 @@ public class Extent extends Mail{
 	public static void end() throws Exception {
 
 		report.flush();		
-		/*sendmailAttachment(filename,"arputhajerrin.e@contus.in");*/		
-
-		/*arputhajerrin.e@contus.in
-		 * dinesh.k@contus.in
-		 * Deprojectsqa@contus.in*/
+		sendmailAttachment(filename,"Mail id");		
 		
 	}
 
